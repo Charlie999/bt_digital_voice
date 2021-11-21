0c85740e427613477e5f503512c4041ca6d72809
Charlie <charlie@i-am.cool>
2021-11-21 21:54:21 +0000

    improved BPF

--- bpf/apply.sh
+++ bpf/apply.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-I1=enp2s0
-I2=enp3s0
+if [ $# -ne 2 ]; then
+	echo "usage: ./apply.sh <if> <mode>"
+	exit 1
+fi
+
+WD="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 function ont() {
  tc qdisc del dev $1 clsact
 
  tc qdisc add dev $1 clsact
- tc filter add dev $1 ingress bpf da obj bt_bpf.o sec bt_pppoe_ingress
- tc filter add dev $1 egress bpf da obj bt_bpf.o sec bt_pppoe_egress
+ tc filter add dev $1 ingress bpf da obj $WD/bt_bpf.o sec bt_pppoe_ingress
+ tc filter add dev $1 egress bpf da obj $WD/bt_bpf.o sec bt_pppoe_egress
 
- tc filter add dev $1 egress bpf da obj bt_ont_bpf.o sec bt_pppoe_ont_egress
+ tc filter add dev $1 egress bpf da obj $WD/bt_ont_bpf.o sec bt_pppoe_ont_egress
 
  echo -e "Added BPF to $1\n==="
 
@@ -25,10 +29,10 @@ function rt() {
  tc qdisc del dev $1 clsact
 
  tc qdisc add dev $1 clsact
- tc filter add dev $1 ingress bpf da obj bt_bpf.o sec bt_pppoe_ingress
- tc filter add dev $1 egress bpf da obj bt_bpf.o sec bt_pppoe_egress
+ tc filter add dev $1 ingress bpf da obj $WD/bt_bpf.o sec bt_pppoe_ingress
+ tc filter add dev $1 egress bpf da obj $WD/bt_bpf.o sec bt_pppoe_egress
 
- tc filter add dev $1 egress bpf da obj bt_rt_bpf.o sec bt_pppoe_rt_egress
+ tc filter add dev $1 egress bpf da obj $WD/bt_rt_bpf.o sec bt_pppoe_rt_egress
 
  echo -e "Added BPF to $1\n==="
 
@@ -39,5 +43,4 @@ function rt() {
 
 }
 
-ont $I1
-rt $I2
+$2 $1
